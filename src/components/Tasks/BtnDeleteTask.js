b84b31be2cfdb8f3247e09688f413ac9cc3fb5f1
Alex Paraschiv <aparaschiv@adobe.com>
2023-06-10 01:50:51 +0300

    added the files for modals once again

--- src/components/Tasks/BtnDeleteTask.js
+++ src/components/Tasks/BtnDeleteTask.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-// import ModalConfirm from "../../Utilities/ModalConfirm";
-import { ReactComponent as Trash } from "../../assets/trash.svg";
+import React, { useState } from 'react';
+import { ReactComponent as Trash } from '../../assets/trash.svg';
+import ConfirmationModal from './ConfirmationModal';
 
 const BtnDeleteTask = ({ taskId }) => {
-  const [showModal, setIsModalShown] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   // const dispatch = useAppDispatch();
 
-  const removeTaskHandler = () => {
-    // dispatch(tasksActions.removeTask(taskId));
+  const deleteTask = () => {
+    setShowModal(false);
   };
   return (
     <>
-      {/* {showModal && (
-        <ModalConfirm
-          onClose={() => setIsModalShown(false)}
-          text="This task will be deleted permanently."
-          onConfirm={removeTaskHandler}
+      {showModal && (
+        <ConfirmationModal
+          onCancel={() => setShowModal(false)}
+          message='This task will be deleted permanently.'
+          onConfirm={deleteTask}
         />
-      )} */}
+      )}
       <button
-        onClick={() => setIsModalShown(true)}
-        title="delete task"
-        className="ml-2 transition hover:text-slate-700 dark:hover:text-slate-200"
+        onClick={() => setShowModal(true)}
+        title='delete task'
+        className='ml-2 transition hover:text-slate-700 dark:hover:text-slate-200'
       >
-        <Trash className="w-5 h-5 sm:w-6 sm:h-6" />
+        <Trash className='w-5 h-5 sm:w-6 sm:h-6' />
       </button>
     </>
   );
 };
 
-export default React.memo(BtnDeleteTask);
+export default BtnDeleteTask;
